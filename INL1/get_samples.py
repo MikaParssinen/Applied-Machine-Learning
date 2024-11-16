767f33e878d4c4661a71b497d04f4105e10ac37c
MikaParssinen <mika.77@live.se>
2024-11-16 11:14:55 +0100

    Fixed split data

--- INL1/get_samples.py
+++ INL1/get_samples.py
@@ -4,8 +4,8 @@ import pandas as pd
 def split_data():
     df = pd.read_csv("./FIFA18_players_database/clean_data.csv", low_memory=False)
     df = df.sample(frac=1).reset_index(drop=True)  # Shuffle the rows of the dataframe
-    X = df.iloc[:, range(1, 36)]
-    y = df.iloc[:, 36]
+    X = df.iloc[:, :-1]  # Only save attributes to X
+    y = df.iloc[:, -1]  # Only save last column in y
 
     num_rows = len(df.index)
     num_train_rows = int(num_rows * 0.8)
