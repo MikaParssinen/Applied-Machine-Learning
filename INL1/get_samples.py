c9bd3f6d95e06556af8242390db02a035a3c8a70
Marcus Hammarström <marham1995@gmail.com>
2024-11-17 20:31:20 +0100

    done with implementing naive bayes classifiers

--- INL1/get_samples.py
+++ INL1/get_samples.py
@@ -8,7 +8,7 @@ def split_data():
     y = df.iloc[:, -1]  # Only save last column in y
 
     num_rows = len(df.index)
-    num_train_rows = int(num_rows * 0.8)
+    num_train_rows = int(num_rows * 0.01)
     num_test_rows = num_rows - num_train_rows
 
     X_train = X.head(num_train_rows)
