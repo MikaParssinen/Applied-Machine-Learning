55bc7c413eb55db50cc8b03ee1830a9292e3a392
MarcusHammarstrom <marham1995@gmail.com>
2024-11-27 10:57:50 +0100

    conflicts resolved

--- INL1/naivebayes.py
+++ INL1/naivebayes.py
@@ -12,4 +12,4 @@ def run_naive_bayes(X_train, X_test, y_train):
     model = GaussianNB()
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    return y_pred
+    return (y_pred, model)
