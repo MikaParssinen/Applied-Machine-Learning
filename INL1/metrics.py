e003546f147becac88f3bcd3a3643299a9285b02
MarcusHammarstrom <marham1995@gmail.com>
2024-11-27 19:12:51 +0100

    move all helper functions to a library of ours

--- INL1/metrics.py
+++
@@ -1,7 +0,0 @@
-def accuracy_scorer(y_test, y_pred):
-    length = len(y_test)
-    count = 0
-    for i in range(length):
-        if y_pred[i] in y_test[i]:
-            count += 1
-    return count/length
--- INL1/naivebayes.py
+++
@@ -1,7 +0,0 @@
-from sklearn.naive_bayes import GaussianNB
-
-def run_naive_bayes(X_train, X_test, y_train):
-    model = GaussianNB()
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    return (y_pred, model)
