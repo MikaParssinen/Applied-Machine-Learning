05399f91b50bbabaa1c789bac37e67cf8a345932
MarcusHammarstrom <marham1995@gmail.com>
2024-11-27 14:41:17 +0100

    implemented cross-validation in split_data

--- INL1/train_test_split.py
+++ INL1/train_test_split.py
@@ -6,28 +6,41 @@ def clean_plus_minus(value):
         return re.sub(r'[+-].*', '',value)
     return value
 
-def split_data():
-    df = pd.read_csv("./FIFA18_players_database/CompleteDataset.csv", low_memory=False)
-    pd.options.mode.copy_on_write = True  # Enable copy on write in pandas  # Open file to clean
-    df = df.reset_index()  # Make sure indexes pair with number of rows
-
-    cols = list(range(14, 48))
-    cols.append(64)
+def engineer_features(df):
+    cols = list(range(13, 46))
+    cols.append(63)
     df = df.iloc[:, cols]  # Extract only columns of interest
-    df = df.sample(frac=1).reset_index(drop=True)  # Shuffle the rows of the
     df = df[~df["Preferred Positions"].str.contains("GK")]  # Remove all goalkeeper rows
     cols_to_drop = [col for col in df.columns if "GK" in col]  # Find all GK attributes
     df.drop(cols_to_drop, axis=1, inplace=True)  # Remove columns
     df = df.map(clean_plus_minus)  # Remove +, -.
+
+    return df
+
+def split_data(df, split):
+    pd.options.mode.copy_on_write = True  # Enable copy on write in pandas
 
     # Calculate 80-20 split of number of rows for training and testing
     num_rows = len(df.index)
     num_train_rows = int(num_rows * 0.8)
     num_test_rows = num_rows - num_train_rows
 
-    # Create training set and test set
-    train_df = df.head(num_train_rows)
-    test_df = df.tail(num_test_rows)
+    # Create training set and test set split
+    if split == 0:
+        test_df = df[:num_test_rows]
+        train_df = df[num_test_rows:]
+    elif split == 1:
+        test_df = df[num_test_rows:num_test_rows * 2]
+        train_df = pd.concat([df[:num_test_rows], df[num_test_rows * 2:]])
+    elif split == 2:
+        test_df = df[num_test_rows * 2:num_test_rows * 3]
+        train_df = pd.concat([df[:num_test_rows * 2], df[num_test_rows * 3:]])
+    elif split == 3:
+        test_df = df[num_test_rows * 3:num_test_rows * 4]
+        train_df = pd.concat([df[:num_test_rows * 3], df[num_test_rows * 4:]])
+    else:
+        test_df = df[num_test_rows * 4:]
+        train_df = df[:num_test_rows * 4]
 
     # Split rows with multiple labels into multiple rows
     train_df['Preferred Positions'] = train_df['Preferred Positions'].str.strip()  # Remove whitespaces
