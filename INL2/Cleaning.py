44902d8fddaeceae1a00602d951fbb83ece7199e
MikaParssinen <mika.77@live.se>
2024-12-11 14:29:36 +0100

    Trying to find clusters

--- INL2/Cleaning.py
+++ INL2/Cleaning.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
+
 df = pd.read_csv("./Dataset/Customer Data.csv")
 
-df.drop_duplicates(inplace=True)
 df.drop(['TENURE', 'CUST_ID'], axis=1, inplace=True)
 df = df.astype({'PURCHASES_TRX': 'float', 'CASH_ADVANCE_TRX': 'float'})
+df.drop_duplicates(inplace=True)
 df.dropna(inplace=True)
 
 df.to_csv("./Dataset/Clean.csv", index=False)
